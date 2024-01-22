747333d587b88dba415134c2ded4eee4b984ea73
yifei.su <phenix863@163.com>
2024-01-22 21:53:21 +0800

    add python code ok base

---
+++ pb01_gui_main.py
@@ -0,0 +1,40 @@
+
+""" step1: 导入必须的库和 layout 文件 """
+import sys
+from PyQt5.QtWidgets import *
+from pb01_gui_layout import Ui_PB01_Demo    # pb01_gui_layout 是 QtDesigner 文件转成的 .py 文件
+                                            # Ui_PB01_Demo 是 QtDesigner 文件转成 .py 文件后生成的类
+from PyQt5.QtCore import *
+
+
+""" step2: 通过继承 layout 文件的类制作 GUI 的类
+           这个类中，要实现下面的内容：
+           1. 控件的初始化
+           2. 控件信号和槽的连接
+           3. 各控件槽函数的实现
+           4. 自定义个函数
+           5. GUI 要实现的其它事项等，比如 HID 的连接
+"""
+class Pb01DemoWindow(QMainWindow, Ui_PB01_Demo):
+    def __init__(self, parent=None):
+        super(Pb01DemoWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.initUI()       # 定义初始化函数
+
+    ''' GUI 初始化函数 '''
+    def initUI(self):
+        self.pushButton_37.clicked.connect(self.btn_handle)     # 定义控件 pushButton_37 单击信号的槽函数为 btn_handle
+
+    ''' 控件 pushButton_37 单击信号槽函数的具体实现 '''
+    def btn_handle(self):
+        print("this is btn slot function")
+
+
+""" step3: 通过下面代码完成 GUI 的显示 """
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    win = Pb01DemoWindow()
+    win.show()
+
+    sys.exit(app.exec_())

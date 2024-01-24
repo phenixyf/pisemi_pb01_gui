aaf4310159b6c598a92353979ae030bede2465c9
yifei.su <phenix863@163.com>
2024-01-24 21:01:11 +0800

    complete dan design tablewidget format ok base

--- dan_ui_main.py
+++ dan_ui_main.py
@@ -16,21 +16,35 @@ class Pb01DanWindow(QMainWindow, Ui_MainWindow):
     ''' GUI 初始化函数 '''
     def initUI(self):
         ''' 初始化 uart interface configure table1 '''
-        for row in range(3):
-            for column in range(14):
-                item = self.tableWidget_uart_if_cfg1.item(row, column)
-                # 单元格内容水平居中对齐
-                if item:  # 确保单元格不为空
-                    item.setTextAlignment(Qt.AlignHCenter | item.textAlignment() & ~Qt.AlignHorizontal_Mask)
-                # 填充单元格背景
-                if column == 3:  # 设置第4列的背景色
+        # 设置列标题
+        column_headers = ["Address", "Register", "Expected {hex}", "Actual {hex}", "UARTDUAL", "UARTLPBK", "UARTWRPATH",
+                          "TXUIDLEHIZ", "TXLIDLEHIZ", "UARTDCEN", "UARTALVCNTEN", "(Logic Zero)", "DBLBUFEN",
+                          "Reserved/SPI[6:0]"]
+
+        self.tableWidget_uart_if_cfg1.horizontalHeader().setDefaultAlignment(Qt.AlignHCenter)   # 设置标题内容水平居中对齐
+        self.tableWidget_uart_if_cfg1.setHorizontalHeaderLabels(column_headers)     # 设置标题栏内容
+
+        # 设置单元格数据
+        data = [
+            ["0x10", "UIFCFG (Single AFE)", "2600", "A410", "0", "0", "1", "0", "0", "1", "1", "0", "0", "0000000"],
+            ["0x10", "UIFCFG (Dual, Device 0)", "2600", "A410", "0", "0", "1", "0", "0", "1", "1", "0", "0", "0000000"],
+            ["0x10", "UIFCFG (Dual, Device 1)", "2600", "A410", "0", "0", "1", "0", "0", "1", "1", "0", "0", "0000000"]
+        ]
+
+        # 填充数据并设置背景色
+        for row, row_data in enumerate(data):
+            for column, value in enumerate(row_data):
+                item = QTableWidgetItem(value)
+                item.setTextAlignment(Qt.AlignHCenter)  # 设置单元格内容水平居中对齐
+                self.tableWidget_uart_if_cfg1.setItem(row, column, item)
+                # 根据条件设置背景色
+                if column == 3:  # 设置第 4 列的背景色
                     item.setBackground(QColor("#b5e6aa"))
-                # if column > 3:  # 设置第4列的背景色
-                #     item.setBackground(QColor("#fff0b3"))
+                if column > 3:  # 设置第 5 ~ 14 列的背景色
+                    item.setBackground(QColor("#fff0b3"))
 
-        self.tableWidget_uart_if_cfg1.resizeColumnsToContents()     # 设置 uart if cfg1 table 随内容自动设置列宽
-
-
+        # 列宽自适应等
+        self.tableWidget_uart_if_cfg1.resizeColumnsToContents()
 
 
 """ step3: 通过下面代码完成 GUI 的显示 """
